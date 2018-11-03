0a280e3b035ce067ad3f41e30c35efbda0972d48
negen <1096041251@qq.com>
2018-11-03 13:03:17 +0800

    1103

--- ComputerSaleSys/src/main/java/com/scs/entity/Product.java
+++ ComputerSaleSys/src/main/java/com/scs/entity/Product.java
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.GeneratorType;
 
 @Entity
 @Table(name = "product")
